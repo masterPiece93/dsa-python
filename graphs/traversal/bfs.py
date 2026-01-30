7e3693814a4a28d28695ab6d57cf6a47d017adbe
masterPiece93 <ankit8290@gmail.com>
2026-01-30 13:02:27 +0530

    graphs : updated the bfs implementation with cli args

--- graphs/traversal/bfs.py
+++ graphs/traversal/bfs.py
@@ -120,7 +120,10 @@ class BfsMixin:
 # ====
 # main
 # ====
-def main(representation: RepresentationOptions = RepresentationOptions.ADJACENCY_LIST):
+def main(
+    representation: RepresentationOptions = RepresentationOptions.ADJACENCY_LIST,
+    graph_name: str = ""
+):
 
     print('\t\t-----')
     print('\t\t BFS')
@@ -136,37 +139,35 @@ def main(representation: RepresentationOptions = RepresentationOptions.ADJACENCY
                         - Type : Adjacency List
                         - Traversal : BFS
                     """
-                
+                # Instantiation
                 graph = Graph()
-                FormulateAdjacencyListGraph.sample_1(graph)
-                print(FormulateAdjacencyListGraph.sample_1.__doc__)
-                print(SAMPLE_1)
-                result = graph.bfs(0)
-                print('\tBFS of Graph 1 : ', result)
-                assert result == '0 -> 1 -> 2 -> 3 -> 4' # expected order of bfs from node 0
 
-                print('\n', '-'*10, '\n')
+                match graph_name:
+                    
+                    case 'SAMPLE_1':
+                        FormulateAdjacencyListGraph.sample_1(graph)
+                        print(FormulateAdjacencyListGraph.sample_1.__doc__)
+                        result = graph.bfs(0)
+                        print('\tBFS of Graph 1 : ', result)
+                        assert result == '0 -> 1 -> 2 -> 3 -> 4' # expected order of bfs from node 0
 
-                graph = Graph()
-                graph = FormulateAdjacencyListGraph.sample_2(graph)
-                print(FormulateAdjacencyListGraph.sample_2.__doc__)
-                print(SAMPLE_2)
-                result = graph.bfs(0)
-                print('\tBFS of Graph 2 : ', result)
-                assert result == '0 -> 2 -> 3 -> 1 -> 4' # expected order of bfs from node 0
+                    case 'SAMPLE_2':
+                        graph = FormulateAdjacencyListGraph.sample_2(graph)
+                        print(FormulateAdjacencyListGraph.sample_2.__doc__)
+                        result = graph.bfs(0)
+                        print('\tBFS of Graph 2 : ', result)
+                        assert result == '0 -> 2 -> 3 -> 1 -> 4' # expected order of bfs from node 0
 
-                print('\n', '-'*10, '\n')
+                    case 'SAMPLE_3':
+                        graph = FormulateAdjacencyListGraph.sample_3(graph)
+                        print(FormulateAdjacencyListGraph.sample_3.__doc__)
+                        result = graph.bfs(0)
+                        print('\tBFS of Graph 3 : ', result)
+                        assert result == '0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6' # expected order of bfs from node 0
 
-                graph = Graph()
-                graph = FormulateAdjacencyListGraph.sample_3(graph)
-                print(FormulateAdjacencyListGraph.sample_3.__doc__)
-                print(SAMPLE_3)
-                result = graph.bfs(0)
-                print('\tBFS of Graph 3 : ', result)
-                assert result == '0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6' # expected order of bfs from node 0
-
-                print('\t\t\n--x--\n')
-        
+                    case _:
+                        raise NotImplemented
+                    
         case RepresentationOptions.ADJACENCY_MATRIX:
 
             class Graph(AdjacencyMatrixGraph, BfsMixin):
@@ -176,37 +177,35 @@ def main(representation: RepresentationOptions = RepresentationOptions.ADJACENCY
                     - Traversal : BFS
                 """
             
-            graph = Graph(total_nodes=5)
-            FormulateAdjacencyMatrixGraph.sample_1(graph)
-            print(FormulateAdjacencyMatrixGraph.sample_1.__doc__)
-            print(SAMPLE_1)
-            result = graph.bfs(0, graph)
-            print('\tBFS of Graph 1 : ', result)
-            expected = '0 -> 1 -> 2 -> 3 -> 4'
-            assert result == expected, f"{expected=} , got {result}" # expected order of bfs from node 0
+            match graph_name:
 
-            print('\n', '-'*10, '\n')
+                case 'SAMPLE_1':
+                    graph = Graph(total_nodes=5)
+                    FormulateAdjacencyMatrixGraph.sample_1(graph)
+                    print(FormulateAdjacencyMatrixGraph.sample_1.__doc__)
+                    result = graph.bfs(0, graph)
+                    print('\tBFS of Graph 1 : ', result)
+                    expected = '0 -> 1 -> 2 -> 3 -> 4'
+                    assert result == expected, f"{expected=} , got {result}" # expected order of bfs from node 0
 
-            graph = Graph(total_nodes=5)
-            graph = FormulateAdjacencyMatrixGraph.sample_2(graph)
-            print(FormulateAdjacencyMatrixGraph.sample_2.__doc__)
-            print(SAMPLE_2)
-            result = graph.bfs(0, graph)
-            print('\tBFS of Graph 2 : ', result)
-            expected='0 -> 1 -> 2 -> 3 -> 4'
-            assert result == expected, f"{expected=} , got {result}" # expected order of bfs from node 0
-            print('\n', '-'*10, '\n')
+                case 'SAMPLE_2':
+                    graph = Graph(total_nodes=5)
+                    graph = FormulateAdjacencyMatrixGraph.sample_2(graph)
+                    print(FormulateAdjacencyMatrixGraph.sample_2.__doc__)
+                    result = graph.bfs(0, graph)
+                    print('\tBFS of Graph 2 : ', result)
+                    expected='0 -> 1 -> 2 -> 3 -> 4'
+                    assert result == expected, f"{expected=} , got {result}" # expected order of bfs from node 0
 
-            graph = Graph(total_nodes=7)
-            graph = FormulateAdjacencyMatrixGraph.sample_3(graph)
-            print(FormulateAdjacencyMatrixGraph.sample_3.__doc__)
-            print(SAMPLE_3)
-            result = graph.bfs(0, graph)
-            print('\tBFS of Graph 3 : ', result)
-            expected='0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6'
-            assert result == expected, f"{expected=} , got {result}" # expected order of bfs from node 0
-            print('\t\t\n--x--\n')
-    
+                case 'SAMPLE_3':
+                    graph = Graph(total_nodes=7)
+                    graph = FormulateAdjacencyMatrixGraph.sample_3(graph)
+                    print(FormulateAdjacencyMatrixGraph.sample_3.__doc__)
+                    result = graph.bfs(0, graph)
+                    print('\tBFS of Graph 3 : ', result)
+                    expected='0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6'
+                    assert result == expected, f"{expected=} , got {result}" # expected order of bfs from node 0
+            
         case RepresentationOptions.EDGE_LIST:
             ...
         
@@ -214,6 +213,34 @@ def main(representation: RepresentationOptions = RepresentationOptions.ADJACENCY
 # ==========
 # entrypoint
 # ==========
+import argparse
+
+parser = argparse.ArgumentParser(description="BFS Traversal Demonstration on sample graphs")
+
+# representation group
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--adjacency-list", "-al", action="store_true", help="Use Adjacency List Representation")
+group.add_argument("--adjacency-matrix", "-am", action="store_true", help="Use Adjacency Matrix Representation")
+# sample graph to use
+supported_sample_graphs = ['1', '2', '3']
+default_graph = supported_sample_graphs[0]
+parser.add_argument("--sample-graph", choices=supported_sample_graphs, default=default_graph, help=f"The sample graph to use ( {', '.join(supported_sample_graphs)} ).")
+
 if __name__ == '__main__':
-    main(representation=RepresentationOptions.ADJACENCY_MATRIX)
+    args = parser.parse_args()
+    use_representation: RepresentationOptions = RepresentationOptions.ADJACENCY_LIST
+    use_graph: str = f'SAMPLE_{args.sample_graph}'
+    if args.adjacency_list:
+        print(f"- Using Graph Representation : {RepresentationOptions.ADJACENCY_LIST.value}")
+        use_representation = RepresentationOptions.ADJACENCY_LIST
+    elif args.adjacency_matrix:
+        print(f"- Using Graph Representation : {RepresentationOptions.ADJACENCY_MATRIX.value}")
+        use_representation = RepresentationOptions.ADJACENCY_MATRIX
+    else:
+        print(f"\n- Using Default Graph Representation i.e {use_representation.value}")
+        pass # use default - ADJACENCY_LIST
+    
+    print(f"\n- Using {use_graph}")
+    print(eval(use_graph))
+    main(representation=use_representation, graph_name=use_graph)
 
